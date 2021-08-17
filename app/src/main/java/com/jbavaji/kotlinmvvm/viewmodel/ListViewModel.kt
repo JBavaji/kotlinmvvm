b177873c4f494c2b0dc9017fdd157598a50dda04
Jayeshgiri Bavaji <jbavaji@gmail.com>
2021-08-17 17:16:13 +0530

    step-2 add: ViewModel setup

--- app/src/main/java/com/jbavaji/kotlinmvvm/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/jbavaji/kotlinmvvm/viewmodel/ListViewModel.kt
@@ -1,6 +1,35 @@
 package com.jbavaji.kotlinmvvm.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.jbavaji.kotlinmvvm.model.Country
 
-class ListViewModel: ViewModel() {
+class ListViewModel : ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(
+            Country("Country - A"),
+            Country("Country - B"),
+            Country("Country - C"),
+            Country("Country - D"),
+            Country("Country - E"),
+            Country("Country - F"),
+            Country("Country - G"),
+            Country("Country - H"),
+            Country("Country - I"),
+            Country("Country - J"),
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
 }
